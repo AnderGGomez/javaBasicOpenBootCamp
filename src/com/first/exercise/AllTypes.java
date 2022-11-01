d58c629c0174773d4e97f56221130737558fd4f4
Anderson Gomez <anderson.gomez@utp.edu.co>
2022-11-01 09:19:56 -0500

    Initial Commit

---
+++ src/com/first/exercise/AllTypes.java
@@ -0,0 +1,27 @@
+package com.first.exercise;
+
+
+
+public class AllTypes {
+    public static void main(String[] args) {
+        boolean invalid = true;
+        byte bit = 110;
+        short shortNum = 1500;
+        int intNum = 600000;
+        long longNum = 485l;
+        double doubleNum = 3.85;
+        float floatNum =  4.1f;
+        char charVar = 'c';
+        String stringVar = "hola mundo";
+
+        System.out.println("value of boolean type var : "+invalid);
+        System.out.println("value of byte type var : "+bit);
+        System.out.println("value of short type var: "+shortNum);
+        System.out.println("value of int type var: "+intNum);
+        System.out.println("value of long type var: "+longNum);
+        System.out.println("value of double type var: "+doubleNum);
+        System.out.println("value of float type var: "+floatNum);
+        System.out.println("value of char type var: "+charVar);
+        System.out.println("value of String type var: "+stringVar);
+    }
+}
